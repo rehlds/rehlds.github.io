3b31e5051cb8e6a05866a324fade92578c492f49
Sergey Shorokhov <wopox1337@ya.ru>
2024-10-24 16:34:40 +0300

    config: fix projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "rehlds", // Usually your GitHub org/user name.
-  projectName: "rehlds", // Usually your repo name.
+  projectName: "rehlds.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
