a2e3db8896fa427e98a5065b59e2a63ac6d2c4db
STAM <stamepicmorg@users.noreply.github.com>
2026-08-21 05:23:24 +0300

    Comment out baseUrl in Docusaurus config
    Comment out the baseUrl configuration in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://rehlds.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+ //  baseUrl: "/",
 
   deploymentBranch: "gh-pages",
   trailingSlash: true,
